652e36a5cf214105c1fb3b9403ab7476b7166964
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-05-12 16:00:17 -0400

    script.js

---
+++ Flex-ception-2/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('body')
+let div = document.createElement('div')
+body.appendChild(div)
+div.classList.add('pdiv')
+let sdiv = document.createElementById('sdiv')
+sdiv.appendChildNext(div)
+
+
+
+
+// Leave the <body> tag empty in your HTML file. 
+// Generate all the boxes using DOM Manipulation
+
+//      HINT: Use for loops
+//      HINT: Use the classes mentioned in the CSS file
+//      HINT: Use the commented out div structure in the HTML for reference
